5c97e433a5f740acc09123184c8b0e02d079b1d0
Guilherme <guilhermenied01@gmail.com>
2021-09-28 12:49:57 -0300

    enter

--- _js/script.js
+++ _js/script.js
@@ -4,11 +4,23 @@ function Entra() {
     
     
 
-    if (usuario.value   == "" || senha.value == "") {
+    if (usuario.value  == "" || senha.value == "") {
         window.alert("Campo de usuário ou senha vazio(s)")
        
     
-    }else{
-        window.alert("Logado com sucesso")
+    }
+    
+}
+
+function Cadastra() {
+    const nome = document.getElementById("nomec")
+    const data = document.getElementById("datanac")
+    const usuarioc = document.getElementById("usuarioc")
+    const senhac = document.getElementById("senhac")
+    
+    if (nome.value  == "" || data.value == "" || usuarioc.value   == "" || senhac.value == "") {
+        window.alert("Algum campo vazio")
+        
+        
     }
 }
